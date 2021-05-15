79ddebd5237eb367818d934cc65619661703478c
Colin Edwards <ColinJhEdwards@gmail.com>
2021-05-15 13:51:30 -0700

    Set up for home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import HomeChart from "../components/HomeChart";
+import PersonalStocks from "../components/PersonalStocks";
+import styled from "styled-components";
 
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <HomeChart />
+      <Line id="line"></Line>
+      <PersonalStocks />
     </div>
   );
 };
+
+const Line = styled.div`
+  width: 100%;
+  height: 3px;
+  background: #a300a3;
+  margin: 1rem 0rem;
+`;
 
 export default Home;
