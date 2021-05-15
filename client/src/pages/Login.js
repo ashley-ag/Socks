f7c96fba09b2c04011beb986bd0af7f299024420
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-15 12:07:35 -0700

    Working through JSX on login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from 'styled-components';
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <StyledDiv>
+      <img src={require()} alt={'logo'}></img>
+      <h2 className='logHeader'>Login</h2>
+      <input
+        placeholder={'Email'}>
+
+      </input>
+
+      <input placeholder={'Password'}>
+
+      </input>
+
+
+
+    </StyledDiv>
   );
 };
+
+const StyledDiv = styled.div`
+  
+  `
 
 export default Login;
