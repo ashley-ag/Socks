eef3a33581e4263073817e2f1fc4cfaff4d7caa5
Colin Edwards <ColinJhEdwards@gmail.com>
2021-05-17 18:08:51 -0700

    added login components to route

--- client/src/App.js
+++ client/src/App.js
@@ -7,63 +7,63 @@ import StockDetail from "./pages/StockDetail";
 import Nav from "./components/Nav";
 import GlobalStyle from "./components/GlobalStyle";
 import { Switch, Route } from "react-router-dom";
-import fire from './firebase';
-import Login from './components/Login';
-import Hero from './components/Hero';
+import fire from "./firebase";
+import Login from "./components/Login";
+import Hero from "./components/Hero";
 
 function App() {
-  const [user, setUser] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState ('');
-  const [passwordError, setPasswordError] = useState('');
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [hasAccount, setHasAccount] = useState(false);
 
   const clearInputs = () => {
-    setEmail('');
-    setPassword('');
+    setEmail("");
+    setPassword("");
   };
 
   const clearErrors = () => {
-    setEmailError('');
-    setPasswordError('');
+    setEmailError("");
+    setPasswordError("");
   };
 
   const handleLogin = () => {
     clearErrors();
     fire
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch(err => {
-      switch (err.code) {
-        case 'auth/invalid-email':
-        case 'auth/user-disabled':
-        case 'auth/user-not-found':
-          setEmailError(err.message);
-          break;
-        case 'auth/wrong-password':
-          setPasswordError(err.message);
-          break;
-      }
-    });
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
   };
 
   const handleSignup = () => {
     clearErrors();
     fire
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .catch(err => {
-      switch (err.code) {
-        case 'auth/email-already-in-use':
-        case 'auth/invalid-email':
-          setEmailError(err.message);
-          break;
-        case 'auth/weak-password':
-          setPasswordError(err.message);
-          break;
-      }
-    });
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
   };
 
   const handleLogout = () => {
@@ -71,19 +71,19 @@ function App() {
   };
 
   const authListener = () => {
-    fire.auth().onAuthStateChanged(user => {
-      if(user){
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
         clearInputs();
         setUser(user);
       } else {
-        setUser('');
+        setUser("");
       }
     });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     authListener();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -100,32 +100,27 @@ function App() {
           <AddStocks />
         </Route>
         <Route path="/login">
-          
-          
+          {user ? (
+            <Hero handleLogout={handleLogout} />
+          ) : (
+            <Login
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+              handleLogin={handleLogin}
+              handleSignup={handleSignup}
+              hasAccount={hasAccount}
+              setHasAccount={setHasAccount}
+              emailError={emailError}
+              passwordError={passwordError}
+            />
+          )}
         </Route>
         <Route path="/:id">
           <StockDetail />
         </Route>
       </Switch>
-
-      {user ? (
-        <Hero 
-          handleLogout={handleLogout}
-        />
-      ) : (
-        <Login 
-            email={email} 
-            setEmail={setEmail} 
-            password={password} 
-            setPassword={setPassword} 
-            handleLogin={handleLogin} 
-            handleSignup={handleSignup}
-            hasAccount={hasAccount}
-            setHasAccount={setHasAccount}
-            emailError={emailError}
-            passwordError={passwordError}
-          />
-      )}
     </div>
   );
 }
