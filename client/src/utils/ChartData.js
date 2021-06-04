0c8cedbc39d0e3551617cc683fd5fc5034703642
Colin Edwards <ColinJhEdwards@gmail.com>
2021-06-03 19:43:13 -0700

    created nostock.js which will prompt the user to add stocks to their profile if none are present on the home page

--- client/src/utils/ChartData.js
+++ client/src/utils/ChartData.js
@@ -1,36 +1,9 @@
-export const personalData = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Units",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
 export const marketData = {
   labels: ["aapl", "goog", "tsla", "msft"],
   datasets: [
     {
       label: "Current Price",
-      data: [125.37, 2343.64, 580.89, 245.1],
+      data: [125.37, 2400.64, 580.89, 245.1],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
